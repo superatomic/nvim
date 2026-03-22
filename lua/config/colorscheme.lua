d0bc0df6e87f7ce634a89a19de2ac536a51032ee
Olivia Kinnear <git@superatomic.dev>
2026-03-21 23:11:48 -0500

    feat(colorscheme): set colorscheme based on 'termguicolors'

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -2,6 +2,15 @@ require 'util.pack' {
   { 'github:catppuccin/nvim', name = 'catppuccin' },
 }
 
-vim.cmd.colorscheme 'catppuccin-macchiato'
-
-vim.api.nvim_set_hl(0, 'VisualCursor', { link = 'MiniStatuslineModeVisual' })
+vim.api.nvim_create_autocmd('OptionSet', {
+  pattern = 'termguicolors',
+  group = vim.api.nvim_create_augroup('config.colorscheme', {}),
+  callback = function()
+    if vim.o.termguicolors then
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+      vim.api.nvim_set_hl(0, 'VisualCursor', { link = 'MiniStatuslineModeVisual' })
+    else
+      vim.cmd.colorscheme 'default'
+    end
+  end,
+})
