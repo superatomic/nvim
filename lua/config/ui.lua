1d94a423ae07acd444d425dc8785e9650d52932c
Olivia Kinnear <git@superatomic.dev>
2026-03-22 23:35:12 -0500

    feat(ui): clamp pager via config

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -1,14 +1,11 @@
+local ui_height = 0.5
+
 require('vim._core.ui2').enable {
   enabled = true,
+  msg = {
+    cmd = { height = ui_height },
+    dialog = { height = ui_height },
+    msg = { height = ui_height },
+    pager = { height = ui_height },
+  },
 }
-
--- See <https://github.com/neovim/neovim/pull/37905#issuecomment-3926500155>.
-vim.api.nvim_create_autocmd('WinEnter', {
-  group = vim.api.nvim_create_augroup('config.ui', {}),
-  callback = function()
-    local max = math.ceil(vim.o.lines * 0.5)
-    if vim.o.filetype == 'pager' and vim.api.nvim_win_get_height(0) > max then
-      vim.api.nvim_win_set_height(0, max)
-    end
-  end,
-})
