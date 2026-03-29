1cfeedf651e9315d3f2234103d5c009197ede863
Olivia Kinnear <git@superatomic.dev>
2026-03-29 12:54:13 -0500

    feat(picker): replace fzf-lua with native `vim.ui.select()`

--- lua/config/picker.lua
+++ lua/config/picker.lua
@@ -1,37 +1,27 @@
--- ------------ --
--- Setup Picker --
--- ------------ --
-
-require 'util.pack' {
-  'github:ibhagwan/fzf-lua',
-}
-
-local fzf = require('fzf-lua')
-fzf.setup {
-  files = {
-    render_crlf = true,
-  },
-}
-
-vim.api.nvim_create_autocmd('TermEnter', {
-  group = vim.api.nvim_create_augroup('config.fzf_keymap', {}),
-  callback = function(info)
-    if vim.bo.filetype == 'fzf' then
-      -- Doing this via the keymap field in FzfLua setup is not documented
-      vim.keymap.set('t', '<S-Tab>', '<Up>', { bufnr = info.buffer })
-      vim.keymap.set('t', '<Tab>', '<Down>', { bufnr = info.buffer })
-    end
-  end,
-})
-
--- ------------ --
--- Set mappings --
--- ------------ --
-
 local map = require('util.map')
 
-map.leader_map('n', 'b', fzf.buffers, 'Buffers')
-
-local filesystem_map = map.leader_group('f', 'Filesystem')
-filesystem_map('n', 'f', fzf.files, 'Open Files')
-filesystem_map('n', 'g', fzf.live_grep, 'Live Grep')
+-- Buffer picker
+map.leader_map('n', 'b', function()
+  local bufs = vim.iter(vim.api.nvim_list_bufs())
+    :filter(function(buf)
+      return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
+    end)
+    :totable()
+  if #bufs < 2 then
+    vim.notify('No other listed buffers', vim.log.levels.WARN)
+    return
+  end
+  vim.ui.select(
+    bufs,
+    {
+      prompt = 'Buffers',
+      format_item = function(item)
+        local path = vim.api.nvim_buf_get_name(item)
+        return vim.fn.fnamemodify(path, ':~:.')
+      end,
+    },
+    function(item)
+      vim.cmd.buffer { item }
+    end
+  )
+end, 'Buffers')
