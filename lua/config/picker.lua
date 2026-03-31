851bc5559cf3df23cbfaad25fe03943ef65df560
Olivia Kinnear <git@superatomic.dev>
2026-03-31 15:57:59 -0500

    feat(picker): use `:find` as file picker

--- lua/config/picker.lua
+++ lua/config/picker.lua
@@ -25,3 +25,18 @@ map.leader_map('n', 'b', function()
     end
   )
 end, 'Buffers')
+
+-- File picker |fuzzy-file-picker|
+vim.cmd([[
+  set findfunc=Find
+  func Find(arg, _)
+    if empty(s:filescache)
+      let s:filescache = globpath('.', '**', 1, 1)
+      call filter(s:filescache, '!isdirectory(v:val)')
+      call map(s:filescache, "fnamemodify(v:val, ':.')")
+    endif
+    return a:arg == '' ? s:filescache : matchfuzzy(s:filescache, a:arg)
+  endfunc
+  let s:filescache = []
+  autocmd CmdlineEnter : let s:filescache = []
+]])
