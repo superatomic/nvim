faeb142ca4c57fddc66820f50fc75d32e800ae2d
Olivia Kinnear <git@superatomic.dev>
2026-05-02 20:01:34 -0500

    feat(lsp): migrate from systemd-language-server to systemd-lsp

--- lsp/systemd.lua
+++ lsp/systemd.lua
@@ -1,7 +1,7 @@
 ---@type vim.lsp.Config
 return {
-  mason = 'systemd-language-server',
-  cmd = { 'systemd-language-server' },
+  mason = 'systemd-lsp',
+  cmd = { 'systemd-lsp' },
   filetypes = { 'systemd' },
   root_markers = { '.git' },
 }
