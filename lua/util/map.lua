f6fd4a5fa4580dbbe270b9a2effbccfc8da5e995
Olivia Kinnear <git@superatomic.dev>
2026-05-30 14:54:09 -0500

    feat(keymap): remove check for table as `lhs`
    Supported upstream as of neovim/neovim#39948.

--- lua/util/map.lua
+++ lua/util/map.lua
@@ -3,10 +3,6 @@ local M = {}
 M.DEFAULT_OPTS = { remap = false, silent = true }
 
 function M._map(mode, lhs, rhs, desc, opts)
-  if type(lhs) == 'table' then
-    vim.iter(lhs):each(function(lhs1) M._map(mode, lhs1, rhs, desc, opts) end)
-    return
-  end
   opts = vim.tbl_extend('keep', opts or {}, { desc = desc }, M.DEFAULT_OPTS)
   vim.keymap.set(mode, lhs, rhs, opts)
 end
