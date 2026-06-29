a77e816cc723cf4660b717cee8d2984cd36fe8ef
Olivia Kinnear <git@superatomic.dev>
2026-06-29 15:54:40 -0500

    feat(sessions): remove support for seamless restart
    Supported upstream as of neovim/neovim#40321.

--- plugin/sessions.lua
+++ plugin/sessions.lua
@@ -1,36 +1,10 @@
--- Seamless :restart
-local first_ui = vim.api.nvim_list_uis()[1]
-local pid = vim.api.nvim_get_chan_info(first_ui.chan).client.attributes.pid
-if not pid then
-  vim.notify(
-    'UI does not support seamless :restart: No provided pid',
-    vim.log.levels.WARN
-  )
-  return
-end
-
-local restart_session_file = vim.fs.joinpath(
-  vim.fn.stdpath('cache') --[[@as string]],
-  'session-' .. pid .. '.tmp.vim'
-)
-
 on('VimLeave', {}, function()
-  local session_file
-  if vim.v.exitreason == 'restart' then
-    session_file = restart_session_file
-  else
+  if vim.v.exitreason == 'normal' then
     -- When exiting normally, save the current session for easy restore
-    session_file = vim.fs.joinpath(
+    local session_file = vim.fs.joinpath(
       vim.fn.stdpath('state') --[[@as string]],
       'session-last.vim'
     )
+    vim.cmd.mksession { session_file, bang = true }
   end
-  vim.cmd.mksession { session_file, bang = true }
-end)
-
-on('UIEnter', { once = true }, function()
-  if vim.v.startreason == 'restart' then
-    vim.cmd.source { restart_session_file }
-  end
-  vim.fs.rm(restart_session_file, { force = true })
 end)
