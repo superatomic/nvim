65b5d18c85f51670955a3496ac9fbf00edbb5afe
Olivia Kinnear <git@superatomic.dev>
2026-07-01 20:36:18 -0500

    feat(format): configure formatters for lua and rust

--- plugin/editor.lua
+++ plugin/editor.lua
@@ -26,6 +26,11 @@ conform.setup {
   default_format_opts = {
     lsp_format = 'fallback',
   },
+  formatters_by_ft = {
+    lua = { 'stylua' },
+    rust = { 'rustfmt' },
+  },
+  log_level = vim.log.levels.WARN,
   notify_on_error = true,
 }
 
