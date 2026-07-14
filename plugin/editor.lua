6d0cf937b6e72ab4e1ebd58ec034b21c50221285
Olivia Kinnear <git@superatomic.dev>
2026-07-14 14:10:51 -0500

    fix(editor): don't remove `:Unlink` command
    Removing `:Unlink` breaks `:Remove`.

--- plugin/editor.lua
+++ plugin/editor.lua
@@ -115,6 +115,6 @@ pack { 'github:tpope/vim-eunuch', name = 'eunuch.vim' }
 -- Fix for https://github.com/tpope/vim-eunuch/issues/121
 map('i', '<CR>', '<CR>')
 
-for _, command in ipairs({ 'Unlink', 'W', 'Wall' }) do
+for _, command in ipairs({ 'W', 'Wall' }) do
   vim.api.nvim_del_user_command(command)
 end
