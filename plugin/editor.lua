bc5cbb788bc93b4845d41999fc598c4707c9733b
Olivia Kinnear <git@superatomic.dev>
2026-07-26 19:28:23 -0500

    feat(editor): integrate trailing whitespace trimming with EditorConfig

--- plugin/editor.lua
+++ plugin/editor.lua
@@ -80,13 +80,17 @@ vim.o.foldlevelstart = 99
 -- Trailing Whitespace --
 -- ------------------- --
 
-on('BufWritePre', { group = 'config.whitespace' }, function()
-  if vim.bo.modified then
+on('BufWritePre', { group = 'config.trim_whitespace' }, function()
+  if (vim.b.editorconfig or {}).trim_trailing_whitespace ~= 'false' then
     local pos = vim.api.nvim_win_get_cursor(0)
     vim.cmd [[keeppatterns %s/\s\+$//e]]
     vim.api.nvim_win_set_cursor(0, pos)
   end
 end)
+
+--- Mask the EditorConfig plugin's `trim_trailing_whitespace` handling.
+--- @diagnostic disable-next-line: duplicate-set-field
+require('editorconfig').properties.trim_trailing_whitespace = function() end
 
 -- ------------------------------ --
 -- Todo Comments and Highlighting --
